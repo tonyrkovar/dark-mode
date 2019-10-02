349e5e2cdb3ca30c0c93a3c2157cca26b338f58e
Tony <tonyrkovar@gmail.com>
2019-10-02 14:33:45 -0500

    set up state

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
+    if (typeof kay !== 'string') {
+        throw new Error('Invalid Params: useLocalStorage should receive a string for the first argument')
+    }
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
-        return
-    })
+        return (item ? JSON.parse(item) : initialValue)
+    });
+
+    return storedValue;
 }
