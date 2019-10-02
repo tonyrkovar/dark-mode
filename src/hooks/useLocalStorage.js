58d09b5b735eff08e12825c55c18a5fa8a601a19
Tony <tonyrkovar@gmail.com>
2019-10-02 14:20:36 -0500

    installed npm and node sass and set up the hooks directory with some code for useLocalStorage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return
+    })
+}
